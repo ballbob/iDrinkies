3aa4eb8b0bfa85b19a05bb82901cb307b44de54d
Robert Ball <robert.ball.1989@gmail.com>
2017-04-20 15:27:12 +0100

    Added mongodb. Finally.

--- controllers/local_pub_router.js
+++ controllers/local_pub_router.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var pubRouter = express.Router()
+var PubQuery = require('../db/PubQuery')
 
 pubRouter.get('/', function(req,res){
   pubQuery.all(function(documents){
---
+++ db/pubQuery.js
@@ -0,0 +1,7 @@
+var MongoClient = require('mongodb').MongoClient
+
+var PubQuery = function(){
+
+}
+
+module.exports = PubQuery
