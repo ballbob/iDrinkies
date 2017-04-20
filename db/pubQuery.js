c8e6ac4ccd2b6869723d8b5851f1b5e3225f7491
Robert Ball <robert.ball.1989@gmail.com>
2017-04-20 16:08:33 +0100

    Added update function to pubQuery.

--- db/pubQuery.js
+++ db/pubQuery.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
+var ObjectId = require('mongodb').ObjectID
 
 var PubQuery = function(url){
   this.url = url
@@ -9,6 +10,20 @@ PubQuery.prototype = {
     MongoClient.connect(this.url,function(err,db){
       if(db){
         var collection = db.collection('pubs')
+
+        collection.find().toArray(function(err,documents){
+          callback(documents)
+        })
+      }
+    })
+  },
+
+  update: function(pubId,field,newData,callback){
+    MongoClient.connect(this.url,function(err,db){
+      if(db){
+        var collection = db.collection('pubs')
+
+        collection.updateOne({_id: ObjectID(pubObjectId)}, {$set:  {field: newData}})
 
         collection.find().toArray(function(err,documents){
           callback(documents)
