48388f48ee7dfadb124211ea7d33a4e71653a816
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-04-20 14:47:22 +0100

    Formatting fixes

--- Team Collaboration Folder/leon-seeds.js
+++ Team Collaboration Folder/leon-seeds.js
@@ -1,97 +1,56 @@
--name
--LatLng
--address
--opening hours
--CC ranking
--review
--special events - times for quiz, karaoke
--image URL
+{
+  name: "The Hanging Bat",
+  address: "133 Lothian RD, Edinburgh EH3 9AB",
+  latlng: 55.9463476,-3.2063564,
+  opening_hours: {
+    monday: "12pm–12am",
+    tuesday: "12pm–12am",
+    wednesday: "12pm–12am",
+    thursday: "12pm–12am",
+    friday: "12pm–1am",
+    saturday: "12pm–1am",
+    sunday: "12pm–12am"
+  },
+  cc_ranking: 8,
+  review: "Great selection of beers, really unique flavours not found elsewhere, no full pints though, expensive",
+  special_events: "From IPA days, blind beer boards and many more. some planned, some very last minute - and not forgetting our monthy meet the brewer.",
+  img: "https://goo.gl/maps/zapacbfpkd22",
+},
 
+{
+  name: "Blue Blazer",
+  address: "2 Spittal St, Edinburgh EH3 9DX",
+  latlng: 55.946089,-3.2030417,
+  opening_hours: {
+    monday: "12pm–12am",
+    tuesday: "12pm–12am",
+    wednesday: "12pm–12am",
+    thursday: "12pm–12am",
+    friday: "12pm–1am",
+    saturday: "12pm–1am",
+    sunday: "12pm–12am"
+  },
+  cc_ranking: 6,
+  review: "Traditional independent bar with a good selection of real ales, not alot of space if you are with a larger group, but cozy",
+  special_events: "Home of the Edinburgh Rum Club",
+  img: "https://goo.gl/maps/SpPGJedGqwG2",
+},
 
-***
-
-The Hanging Bat
-
-@55.9463476,-3.2063564
-
-133 Lothian Rd, Edinburgh EH3 9AB
-
-Monday : 12pm–12am
-
-Tuesday : 12pm–12am
-
-Wednesday : 12pm–12am
-
-Thursday : 12pm–12am
-
-Friday : 12pm–1am
-
-Saturday : 12pm–1am
-
-Sunday : 12pm–12am
-
-8 Pink Elephants
-
-"Great selection of beers, really unique flavours not found elsewhere, no full pints though, expensive"
-
-"From IPA days, blind beer boards and many more. Some planned, some very last minute - and not forgetting our monthy Meet The Brewer."
-
-https://goo.gl/maps/ZaPACBFpKD22
-
-***
-
-
-Blue Blazer
-
-2 Spittal St, Edinburgh EH3 9DX
-
-Monday : 11am–1am
-
-Tuesday : 11am–1am
-
-Wednesday : 11am–1am
-
-Thursday : 11am–1am
-
-Friday : 11am–1am
-
-Saturday : 11am–1am
-
-Sunday : 12:30pm–1am
-
-6 Pink Elephants
-
-"Traditional independent bar with a good selection of real ales, not alot of space if you are with a larger group, but cozy"
-
-"Home of the Edinburgh Rum Club"
-
-https://goo.gl/maps/SpPGJedGqwG2
-
-***
-
-
-The Chanter
-
-30-32 Bread St, Edinburgh EH3 9AF
-
-Monday : 11am–1am
-
-Tuesday : 11am–1am
-
-Wednesday : 11am–1am
-
-Thursday : 11am–1am
-
-Friday : 11am–1am
-
-Saturday : 11am–1am
-
-Sunday : 12:30pm–1am
-
-7 Pink Elephants
-
-"Traditional sports bar serving a sizeable menu of pub grub."
-
-"Can be booked for functions and reserve a table for groups"
-
-https://goo.gl/maps/zvBC4pJmPc82
+{
+  name: "The Chanter",
+  address: "30-32 Bread St, Edinburgh EH3 9AF",
+  latlng: 55.946089,-3.2030417,
+  opening_hours: {
+    monday: "11am–1am",
+    tuesday: "11am–1am",
+    wednesday: "11am–1am",
+    thursday: "11am–1am",
+    friday: "11am–1am",
+    saturday: "11am–1am",
+    sunday: "12:30pm–1am"
+  },
+  cc_ranking: 7,
+  review: "Traditional sports bar serving a sizeable menu of pub grub.",
+  special_events: "Can be booked for functions and reserve a table for groups",
+  img: "https://goo.gl/maps/zvBC4pJmPc82",
+}
