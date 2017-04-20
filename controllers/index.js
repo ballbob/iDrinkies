1fa5e6fad03b618529eac26639dff83c1b296cf9
Robert Ball <robert.ball.1989@gmail.com>
2017-04-20 15:36:23 +0100

    Router for local API works. Works for pubQuery.all, showing the entire API.

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 var path = require('path')
 
-router.use('./api/pubs', require('./local_pub_router'))
+router.use('/api/pubs', require('./local_pub_router'))
 
 router.get('/',function(req,res){
   console.log("No main page yet.")
