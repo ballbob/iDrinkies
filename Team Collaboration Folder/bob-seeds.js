e4e8588301d8608c36e3083b0bc49fc992df6aa6
Robert Ball <robert.ball.1989@gmail.com>
2017-04-20 14:41:07 +0100

    Added Bob's seed data.

---
+++ Team Collaboration Folder/bob-seeds.js
@@ -0,0 +1,50 @@
+{
+name: "Shakespeare",
+phone: "0131 228 8400",
+address: "65 Lothian Rd, Edinburgh EH1 2DJ",
+latlng: "@55.9463536,-3.2063671",
+opening_hours: "11:00 - 23:00",
+food_serving_times: "11:00-23:00",
+cc_ranking: "7/10",
+review: "One of the staff punched me",
+special_events: [],
+img: "https://lh4.googleusercontent.com/-xmxzqjCDIJY/WLLYa5a8BzI/AAAAAAAAAPE/OOnnz2zDSh0EgI5kJV7uVPQn8T0TIYwWACLIB/w408-h229-k-no/"
+},
+
+{
+name: "Beer Kitchen",
+address: "81-83 Lothian Rd, Edinburgh EH3 9AW",
+LatLng: "@55.9467485,-3.2069545",
+opening_hours: "09:00-01:00",
+food_serving_hours: "09:00-22:00",
+cc_ranking: "10/10",
+review: "Nice cocktails, though there's beer too. - Adam",
+special_events: ["Fierce Beer Tap Takeover - 26th April 2017, 18:30-20:30."],
+img: "https://lh3.googleusercontent.com/-tJ6DCj89oV4/V8UnwQdnktI/AAAAAAAAaPQ/XYt3BXMYg3suasn-iadwawPIUBArxkjiQCJkC/w408-h229-k-no/"
+},
+
+{
+name: "The Caley",
+phone: "0131 656 0752",
+address: "31 Lothian Road, Edinburgh EH1 2DJ",
+LatLng: "@55.9467485,-3.2069545",
+opening_hours: "08:00-01:00",
+food_serving_hours: "08:00-23:00",
+cc_ranking: "2/10",
+review: "I'll never, ever go here and never have. - Ewen",
+special_events: [],
+img: "https://lh6.googleusercontent.com/-Ydw621RpCFw/WIkoaoXvVVI/AAAAAAAABM0/iEH7ZqaMkV8GfrqVSpw46SMymsY5nFJ7wCLIB/w408-h229-k-no/"
+},
+
+{
+name: "The Red Squirrel",
+phone: "0131 229 9933",
+address: "21 Lothian Road, Old Town, Edinburgh EH1 2DJ", 
+LatLng: "@55.947121,-3.2060694",
+opening_hours: "09:00-00:00",
+food_serving_hours: "09:00-00:00",
+cc_ranking: "5000",
+review: "It's extinct. - Bob",
+special_events: [],
+image: "https://upload.wikimedia.org/wikipedia/commons/1/1c/Squirrel_posing.jpg"
+}
