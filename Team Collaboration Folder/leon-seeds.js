2b0c22ee96aee0b2408929c5dfe552c4ba11f619
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-04-20 14:25:49 +0100

    Leon's seeds.js fIle

---
+++ Team Collaboration Folder/leon-seeds.js
@@ -0,0 +1,97 @@
+-name
+-LatLng
+-address
+-opening hours
+-CC ranking
+-review
+-special events - times for quiz, karaoke
+-image URL
+
+
+***
+
+The Hanging Bat
+
+@55.9463476,-3.2063564
+
+133 Lothian Rd, Edinburgh EH3 9AB
+
+Monday : 12pm–12am
+
+Tuesday : 12pm–12am
+
+Wednesday : 12pm–12am
+
+Thursday : 12pm–12am
+
+Friday : 12pm–1am
+
+Saturday : 12pm–1am
+
+Sunday : 12pm–12am
+
+8 Pink Elephants
+
+"Great selection of beers, really unique flavours not found elsewhere, no full pints though, expensive"
+
+"From IPA days, blind beer boards and many more. Some planned, some very last minute - and not forgetting our monthy Meet The Brewer."
+
+https://goo.gl/maps/ZaPACBFpKD22
+
+***
+
+
+Blue Blazer
+
+2 Spittal St, Edinburgh EH3 9DX
+
+Monday : 11am–1am
+
+Tuesday : 11am–1am
+
+Wednesday : 11am–1am
+
+Thursday : 11am–1am
+
+Friday : 11am–1am
+
+Saturday : 11am–1am
+
+Sunday : 12:30pm–1am
+
+6 Pink Elephants
+
+"Traditional independent bar with a good selection of real ales, not alot of space if you are with a larger group, but cozy"
+
+"Home of the Edinburgh Rum Club"
+
+https://goo.gl/maps/SpPGJedGqwG2
+
+***
+
+
+The Chanter
+
+30-32 Bread St, Edinburgh EH3 9AF
+
+Monday : 11am–1am
+
+Tuesday : 11am–1am
+
+Wednesday : 11am–1am
+
+Thursday : 11am–1am
+
+Friday : 11am–1am
+
+Saturday : 11am–1am
+
+Sunday : 12:30pm–1am
+
+7 Pink Elephants
+
+"Traditional sports bar serving a sizeable menu of pub grub."
+
+"Can be booked for functions and reserve a table for groups"
+
+https://goo.gl/maps/zvBC4pJmPc82
