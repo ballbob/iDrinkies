798270b8da3445df5eac405ce658e5c48dd38d98
Robert Ball <robert.ball.1989@gmail.com>
2017-04-20 15:33:32 +0100

    Added pubQuery, to interact with mongo (populated with our own seeds file, not the google API).

--- controllers/local_pub_router.js
+++ controllers/local_pub_router.js
@@ -2,6 +2,8 @@ var express = require('express')
 var pubRouter = express.Router()
 var PubQuery = require('../db/PubQuery')
 
+pubQuery = new PubQuery('mongodb://localhost:27017/idrinkies')
+
 pubRouter.get('/', function(req,res){
   pubQuery.all(function(documents){
     res.json(documents)
