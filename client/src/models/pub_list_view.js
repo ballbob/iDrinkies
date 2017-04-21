d8dbb7b0c2c0a2d66690e0648ed3508cfb4f397b
Robert Ball <robert.ball.1989@gmail.com>
2017-04-21 11:42:32 +0100

    Pub list view adds an image link, review and opening hours.

--- client/src/models/pub_list_view.js
+++ client/src/models/pub_list_view.js
@@ -7,13 +7,31 @@ PubListView.prototype = {
     for (i=0; i<pubs.length; i++){
       var pubDiv = document.createElement('div')
 
+      var pubImg = document.createElement('a')
+      pubImg.href = pubs[i].img
+      pubImg.innerText = "Have a look at " + pubs[i].name
+      pubDiv.appendChild(pubImg)
+
       var pubName = document.createElement('p')
-      pubName.innerText = pubs[i].name
+      pubName.innerText = "Pub: " + pubs[i].name
       pubDiv.appendChild(pubName)
 
       var pubAddress = document.createElement('p')
-      pubAddress.innerText = pubs[i].address
+      pubAddress.innerText = "Address: " +pubs[i].address
       pubDiv.appendChild(pubAddress)
+
+      var pubOpeningHrs = document.createElement('p')
+      pubOpeningHrs.innerText = "Opening hours: " + pubs[i].opening_hours
+      pubDiv.appendChild(pubOpeningHrs)
+
+      // var pubFoodHrs = document.createElement('p')
+      // pubFoodHrs.innerText = "Food served from: " + pubs[i]
+      // food_serving_hours
+      // pubDiv.appendChild(pubFoodHrs)
+
+      var pubReview = document.createElement('p')
+      pubReview.innerText = "Review: " + pubs[i].review
+      pubDiv.appendChild(pubReview)
 
       this.listElement.appendChild(pubDiv)
     }
