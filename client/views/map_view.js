374cd12a97eceeb162f99a9349079ec091cf761b
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-04-21 11:51:10 +0100

    Progress so far

--- client/views/map_view.js
+++ client/views/map_view.js
@@ -1,12 +1,14 @@
 var initialize = function () {
+
   var mapSection = document.getElementById('map');
   var near-me-button = document.querySelector("#near-me-button");
 
   var mainMap = new MapWrapper(mapDiv, center, 10);
+
   mainMap.addMarker(center);
 
   var findlocation = function () {
     mainMap.geoLocate();
-  }
+  };
 
 }
