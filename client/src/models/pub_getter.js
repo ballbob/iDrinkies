5682356e6a1c1400ea305a3e089042abdaa19e20
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-04-22 21:19:39 +0100

    Minor changes to comments

--- client/src/models/pub_getter.js
+++ client/src/models/pub_getter.js
@@ -8,11 +8,11 @@ var PubGetter = function(url){
 PubGetter.prototype = {
 
   getData: function(callback){
-    var request = new XMLHttpRequest() //start your XMLHttpRequest
-    request.open('GET',this.url) //get from the URL specified in the constructor
-    request.onload = function(){ //once that data has been got, put the data into the pubs array
+    var request = new XMLHttpRequest() // Start your XMLHttpRequest
+    request.open('GET',this.url) // Get from the URL specified in the constructor
+    request.onload = function(){ // Once that data has been got, put the data into the pubs array
       if(request.status === 200){
-        var jsonString = request.responseText //our pubs are listed in the responseText for the request. So, 'de-stringify' it to get the javascript object back.
+        var jsonString = request.responseText // Our pubs are listed in the responseText for the request. So, 'de-stringify' it to get the javascript object back.
         var pubsInfo = JSON.parse(jsonString)
         this.convertToPubObjects(pubsInfo)
         callback(this.pubs)
@@ -28,7 +28,7 @@ PubGetter.prototype = {
       this.pubs.push(pub)
     }.bind(this))
   }
-  
+
 }
 
 module.exports = PubGetter
