d3a8465269049b9dada54939ee2f40493b3207f8
Robert Ball <robert.ball.1989@gmail.com>
2017-04-22 22:24:22 +0100

    Draft search function and event listener.

--- client/src/models/map_wrapper.js
+++ client/src/models/map_wrapper.js
@@ -2,10 +2,15 @@
 
 var MapWrapper = function ( container , coords , zoom ) {
   this.googlemap = new google.maps.Map( container , { center: coords , zoom: zoom })
+  
   //searchbox - formed on loading. Set out the HTML element for the box
   var boxElement = document.querySelector('#search-box')
   //make the box with your parameters
   this.searchBox = new google.maps.places.SearchBox(boxElement)
+
+  //search button
+  this.searchButton = document.querySelector('#submit-button')
+  this.searchButton.onclick = this.search(this.searchBox.value)
 }
 
 MapWrapper.prototype = {
@@ -30,7 +35,20 @@ MapWrapper.prototype = {
       this.addMarker( crds );
     }.bind(this));
   }
-}
+},
+
+  search: function(searchTerm){
+    var userInput = searchTerm
+    var geocoder = new google.maps.Geocoder()
+
+    geocoder.geocode({address: searchTerm}, function(results,status){
+      if(status === 200){
+        this.googlemap.setCenter(results[0].geometry.location)
+      }else{
+        console.log('Search term produced no results')
+      }
+    })
+  }
 
 }
 
