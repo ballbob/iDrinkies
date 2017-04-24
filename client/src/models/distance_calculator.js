9af0ec3c65bd4da82c071aba107229ff0ff262d1
Suzanne <suzanne.aitchison@gmail.com>
2017-04-24 12:10:26 +0100

    removed some unnecessary lines

--- client/src/models/distance_calculator.js
+++ client/src/models/distance_calculator.js
@@ -1,21 +1,21 @@
 var DistanceCalculator = function(){
-
 }
 
 DistanceCalculator.prototype = {
   calculateDistance: function(originCoords, destinationCoords){
-
     var service = new google.maps.DistanceMatrixService()
 
     service.getDistanceMatrix({
-        origins: [new google.maps.LatLng(originCoords.lat, originCoords.lng)],
-        destinations: [new google.maps.LatLng(destinationCoords.lat, destinationCoords.lng)],
-        travelMode: 'WALKING'}, function(response){
-          var origins = response.originAddresses;
-          var destinations = response.destinationAddresses;
-          var distance = response.rows[0].elements[0].distance.text
-          console.log(distance)
-      })
+      origins: [new google.maps.LatLng(originCoords.lat, originCoords.lng)],
+      destinations: [new google.maps.LatLng(destinationCoords.lat, destinationCoords.lng)],
+      travelMode: 'WALKING'}, function(response){
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
+        var distance = response.rows[0].elements[0].distance.text
+        console.log(distance)
+        
+        //ADD A CALLBACK TO PARAMS, AND CALL IT NOW WITH THE DISTANCE INFO
+    })
   }
 }
 
