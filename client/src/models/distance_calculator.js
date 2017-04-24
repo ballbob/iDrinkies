e243410542e2c96f9a577731f2c7f03c6b742006
Suzanne <suzanne.aitchison@gmail.com>
2017-04-24 12:08:25 +0100

    refactored to avoid CORS error

--- client/src/models/distance_calculator.js
+++ client/src/models/distance_calculator.js
@@ -4,25 +4,18 @@ var DistanceCalculator = function(){
 
 DistanceCalculator.prototype = {
   calculateDistance: function(originCoords, destinationCoords){
-  
-    var destinations = 'destinations=' + destinationCoords.lat + ',' + destinationCoords.lng
-    console.log('destinations', destinations)
 
-    var reqUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + originCoords.lat + ',' + originCoords.lng + '&' + destinations + '&key=AIzaSyAMJHPbORbz_vlVN9nNu_wN-jyT02mKnAQ'
-    console.log(reqUrl)
+    var service = new google.maps.DistanceMatrixService()
 
-    var request = new XMLHttpRequest()
-    request.open('GET', reqUrl)
-    request.onload = function(){
-      console.log(request.status)
-      if (request.status === 200){
-        var response = request.responseText
-        var data = JSON.parse(response)
-        var distance = data.rows[0].elements[0].distance.text
-        console.log(distance)
-      }
-    }
-    request.send()
+    service.getDistanceMatrix({
+        origins: [new google.maps.LatLng(originCoords.lat, originCoords.lng)],
+        destinations: [new google.maps.LatLng(destinationCoords.lat, destinationCoords.lng)],
+        travelMode: 'WALKING'}, function(response){
+          var origins = response.originAddresses;
+          var destinations = response.destinationAddresses;
+          var distance = response.rows[0].elements[0].distance.text
+          console.log(distance)
+      })
   }
 }
 
