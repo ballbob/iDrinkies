d713991e6ae88098b785aacb012cd9c6cae60f04
Robert Ball <robert.ball.1989@gmail.com>
2017-04-26 10:28:03 +0100

    Removed extraneous console log.

--- client/src/models/map_wrapper.js
+++ client/src/models/map_wrapper.js
@@ -102,7 +102,6 @@ MapWrapper.prototype = {
         if (correctDiv.childNodes.length <= 2){
           pubLister.dropDownInfo(pub,correctDiv)
         } else {
-          console.log('collapsing')
           pubLister.removeDropDownInfo(correctDiv)
         }
         })
