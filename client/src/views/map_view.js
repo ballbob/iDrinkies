1c6cbffbcbc3604e3b85067dd2bc348fd840af1e
Robert Ball <robert.ball.1989@gmail.com>
2017-04-26 09:28:41 +0100

    Changed initial zoom and location.

--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -9,7 +9,7 @@ MapView.prototype = {
 
   initialize: function () {
     var mapSection = document.getElementById('map-content');
-    var center = { lat: 0 , lng: 0 }
+    var center = { lat: 55.950333, lng: -3.199003}
     this.mainMap = new MapWrapper( mapSection, center, 16 );
     this.mainMap.geolocate();
   },
